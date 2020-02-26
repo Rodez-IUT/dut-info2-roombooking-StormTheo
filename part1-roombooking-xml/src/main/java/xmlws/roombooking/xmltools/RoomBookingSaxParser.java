2a1043282f6b2408b983c22d5ac9ca5050ff2bb6
Théo Bouchouieff <theo.bouchouieff@iut-rodez.fr>
2020-02-26 09:27:54 +0100

    commit de save

---
+++ part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingSaxParser.java
@@ -0,0 +1,38 @@
+package xmlws.roombooking.xmltools;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.InputStream;
+
+public class RoomBookingSaxParser implements RoomBookingParser {
+
+
+    @Override
+    public RoomBooking parse(InputStream inputStream) {
+        try {
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            spf.setNamespaceAware(true);
+            SAXParser saxParser = spf.newSAXParser();
+            saxParser.parse(inputStream, new RoomBookingBasicHandler());
+
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+
+
+    private class RoomBookingBasicHandler extends DefaultHandler {
+
+        public void startElement(String namespaceURI,
+                                 String localName,
+                                 String qName,
+                                 Attributes atts)
+                throws SAXException {
+            System.out.println("In element: " + localName);
+        }
+    }
+
+}
